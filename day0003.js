8cfc9f0188089682618ca8c6c901e1ba70e8debc
Nguyen Le <117344724+chaleo@users.noreply.github.com>
2022-12-09 13:58:33 +0700

    Create day0003.js

---
+++ day0003.js
@@ -0,0 +1,21 @@
+<script>
+document.write(5 + 6 + "e");
+</script>
+
+<script>
+document.getElementById("demo").innerHTML = 7 + 6 + "a";
+</script>
+
+<script>
+alert("what is an alert?");
+alert("john cena");
+alert("bing chiling");
+</script>
+
+<script>
+console.log("BING CHILING");
+</script>
+
+<p>Click the button to print the current page.</p>
+
+<button onclick="print()">Print this page</button>
