fb22c753f94b08eeb072be093ca9235f2d33483d
Nguyen Le <117344724+chaleo@users.noreply.github.com>
2022-12-13 14:52:24 +0700

    Update day0005.js

--- day0005.js
+++ day0005.js
@@ -8,3 +8,19 @@ function myFunction() {
   "Cookies associated with this document: " + document.cookie;
 }
 </script>
+
+<p id="demo2"></p>
+
+<script>
+let x = 100 + 50;
+document.getElementById("demo2").innerHTML = x;
+</script>
+
+<p id="demo3"></p>
+
+<script>
+let a = 5;
+let b = 2;
+let c = a + b;
+document.getElementById("demo3").innerHTML = c;
+</script>
