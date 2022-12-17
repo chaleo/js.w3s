a3b3689519525e8e5bae5a441e3c4f26baf4ed8c
Nguyen Le <117344724+chaleo@users.noreply.github.com>
2022-12-17 21:22:27 +0700

    Update day0007.js

--- day0007.js
+++ day0007.js
@@ -27,3 +27,18 @@ z = "John";    // Now z is a String
 
 document.getElementById("demo3").innerHTML = z;
 </script>
+
+<p>You can use quotes inside a string, as long as they don't match the quotes surrounding the string:</p>
+
+<p id="demo4"></p>
+
+<script>
+let answer1 = "It's alright";
+let answer2 = "He is called 'Johnny'";
+let answer3 = 'He is called "Johnny"';
+
+document.getElementById("demo4").innerHTML =
+answer1 + "<br>" + 
+answer2 + "<br>" + 
+answer3;
+</script>
