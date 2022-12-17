fdf2aaea69d5b36d475d10dfce20401fc09419d4
Nguyen Le <117344724+chaleo@users.noreply.github.com>
2022-12-17 21:17:18 +0700

    Update day0007.js

--- day0007.js
+++ day0007.js
@@ -12,7 +12,18 @@ document.getElementById("demo").innerHTML = x;
 <p id="demo2"></p>
 
 <script>
-let x = "Volvo" + 16;
-document.getElementById("demo2").innerHTML = x;
+let y = "Volvo" + 16;
+document.getElementById("demo2").innerHTML = y;
 </script>
 
+<p>JavaScript has dynamic types. This means that the same variable can be used to hold different data types:</p>
+
+<p id="demo3"></p>
+
+<script>
+let z;         // Now z is undefined
+z = 5;         // Now z is a Number
+z = "John";    // Now z is a String
+
+document.getElementById("demo3").innerHTML = z;
+</script>
