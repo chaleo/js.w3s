48008fa2b644f76ac049f562148b93e23d146a1a
Nguyen Le <117344724+chaleo@users.noreply.github.com>
2022-12-19 22:18:09 +0700

    Create day0008.js

---
+++ day0008.js
@@ -0,0 +1,11 @@
+<p>This example calls a function which performs a calculation, and returns the result:</p>
+
+<p id="demo0"></p>
+
+<script>
+function myFunction(p1, p2) {
+  return p1 * p2;
+}
+document.getElementById("demo0").innerHTML = myFunction(4, 3);
+</script>
+
