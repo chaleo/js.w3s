357117894c3269f6b86f8f18befe2dee2c1d0843
Nguyen Le <117344724+chaleo@users.noreply.github.com>
2022-12-27 14:38:38 +0700

    Create day0009.js

---
+++ day0009.js
@@ -0,0 +1,10 @@
+<h2>JavaScript Variables</h2>
+
+<p id="demo1"></p>
+
+<script>
+// Create and display a variable:
+let car = "Fiat";
+document.getElementById("demo1").innerHTML = car;
+</script>
+
