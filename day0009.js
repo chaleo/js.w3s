a8e491b45a2009603eeadd5d993cb92cdd1b8fc7
Nguyen Le <117344724+chaleo@users.noreply.github.com>
2022-12-27 14:40:16 +0700

    Update day0009.js

--- day0009.js
+++ day0009.js
@@ -8,3 +8,15 @@ let car = "Fiat";
 document.getElementById("demo1").innerHTML = car;
 </script>
 
+<h2>JavaScript Objects</h2>
+
+<p id="demo2"></p>
+
+<script>
+// Create an object:
+const car = {type:"Fiat", model:"500", color:"white"};
+
+// Display some data from the object:
+document.getElementById("demo2").innerHTML = "The car type is " + car.type;
+</script>
+
