addec6ee8d046a010a0d1c3a98ae3709be08bbfc
Nguyen Le <117344724+chaleo@users.noreply.github.com>
2022-12-27 14:50:52 +0700

    Update day0009.js

--- day0009.js
+++ day0009.js
@@ -51,3 +51,23 @@ document.getElementById("demo4").innerHTML =
 person.firstName + " is " + person.age + " years old. " + "He has " + person.eyeColor + " eyes."
 </script>
 
+
+<h2>JavaScript Objects</h2>
+<p>An object method is a function definition, stored as a property value.</p>
+
+<p id="demo5"></p>
+
+<script>
+// Create an object:
+const person = {
+  firstName: "John",
+  lastName: "Doe",
+  id: 5566,
+  fullName: function() {
+    return this.firstName + " " + this.lastName;
+  }
+};
+
+// Display data from the object:
+document.getElementById("demo5").innerHTML = person.fullName();
+</script>
