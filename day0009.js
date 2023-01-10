a7fe912fd6b8ddeb3e0c43824dbe39ee3b2380fb
Nguyen Le <117344724+chaleo@users.noreply.github.com>
2023-01-10 18:03:55 +0700

    Update day0009.js

--- day0009.js
+++ day0009.js
@@ -89,3 +89,19 @@ document.getElementById("demo5").innerHTML = person.fullName();
 
 </body>
 
+<body>
+
+<h2>JavaScript HTML Events</h2>
+<p>Click the button to display the date.</p>
+
+<button onclick="displayDate()">The time is?</button>
+
+<script>
+function displayDate() {
+  document.getElementById("demo0110b").innerHTML = Date();
+}
+</script>
+
+<p id="demo0110b"></p>
+
+</body>
